0e1b98e103fd1ac90a97792f0e1911ada3a5dc3d
CamilleLanglais <123119438+CamilleLanglais@users.noreply.github.com>
2023-04-26 20:18:46 +0200

    makefile

--- src/corridor.c
+++ src/corridor.c
@@ -6,20 +6,30 @@
 void drawCorridor(){
     //sol
     glPushMatrix();
-		glTranslatef(0.,0.,-2.5);
+		glTranslatef(0.,0.,-5.);
 		glScalef(30.0,10.0,-2.5);
 		glColor3f(0.0,0.0,0.5);
 		drawSquare();
 		glPopMatrix();
 
-    //Mur
+    //Mur gauche
 	glPushMatrix();
 		glTranslatef(-5., -2., 0.);
-		glScalef(30., 10.,4.);
+		glScalef(30., 10., 4.);
 		glRotatef(90.,1.,0.,0.);
 		glColor3f(0.,0.5,0.5);
 		drawSquare();
     glPopMatrix();
+
+// mur droit
+	glPushMatrix();
+		glTranslatef(-5., -2., 0.);
+		glScalef(30., 10., 4.);
+		glRotatef(180.,1.,0., 0.);
+		glColor3f(0.,0.5,0.5);
+		drawSquare();
+    glPopMatrix();
+
 
     //plafond
     glPushMatrix();
